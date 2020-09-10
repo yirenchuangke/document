cca49b39f5de8994f19d9da7964cc0a1b4cb431d
liushuanglong <13522002566@163.com>
2020-09-10 23:00:54 +0800

    初始化，安装antd

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
-
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+ import Vue from "vue";
+ import App from "./App.vue";
+ import "./registerServiceWorker";
+ import router from "./router";
+ import store from "./store";
+ import { Button } from 'ant-design-vue';
+ Vue.config.productionTip = false;
+ Vue.component(Button.name, Button)
+ new Vue({
+   router,
+   store,
+   render: h => h(App)
+ }).$mount("#app");
